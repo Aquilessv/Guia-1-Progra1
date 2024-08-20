0f3a2b903975690329e183ed6bac09ae3ee9d81e
Brenda Ivania <bivania706@gmail.com>
2024-08-20 17:12:29 -0600

    Update Ejercicio1.cs

--- Ejercicio1.cs
+++ Ejercicio1.cs
@@ -1,46 +1,41 @@
-﻿using System.ComponentModel.Design;
-using static System.Runtime.InteropServices.JavaScript.JSType;
-
-class Ejercicio1 { 
-
-    public static void Main (string[] args) {
-
-
+class Ejercicio1
+{
+    public static void Main(string[] args)
+    {
         Console.WriteLine("Digite dos numeros porfavor:");
 
         int num1 = 0;
         int num2 = 0;
         int numero;
-      
-
 
         Console.WriteLine("Digite el primer numero");
         num1 = int.Parse(Console.ReadLine());
-        Console.WriteLine("porfavor digite el segundo numero");
+        Console.WriteLine("Porfavor digite el segundo numero");
         num2 = int.Parse(Console.ReadLine());
 
         int numeroM = Math.Max(num1, num2);
+
         Console.WriteLine("El numero mayor es :" + " " + numeroM);
-
-        Console.WriteLine("======================================================================");
-
-      
+        Console.WriteLine("======================================================================\n");
         Console.WriteLine("Digite un numero entero positivo que quiera sacarle raiz cubica");
 
-        while(true)
+        while (true)
         {
             string NumeroI = Console.ReadLine();
-            if(int.TryParse(NumeroI, out numero) && numero >0 ) {
+            if (int.TryParse(NumeroI, out numero) && numero > 0)
+            {
                 break;
             }
             else
             {
-                Console.WriteLine("EL dato es invalido porfavor ingrese un numero entero positvo");
+                Console.WriteLine("EL dato es invalido porfavor ingrese un numero entero positivo");
             }
         }
 
-        double raiz= Math.Pow(numero,1.0/3.0 );
+        double raiz = Math.Pow(numero, 1.0 / 3.0);
+        Console.WriteLine("La raiz cubica de" + " " + numero + " " + "es aproximadamente" + " " + raiz);
 
-        Console.WriteLine("La raiz cubica de"+" "+ numero + " "+"es aproximadamente"+" "+raiz);
+        Console.ReadKey();
     }
 }
+
