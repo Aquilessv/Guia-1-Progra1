a5d6ae8f947c184813a1b63ea814044c6b9a0905
DamarisGaray <damagarai@gmail.com>
2024-08-19 21:48:48 -0600

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿class Ejerccio3
+{
+    static void Main(string[] args)
+    {
+
+        //solicitar al usuario ingresar un numero
+        Console.WriteLine("Ingrese un numero");
+        string num = Console.ReadLine();
+        int numero;
+        //int numero = int.Parse(Console.ReadLine());
+
+        if (String.IsNullOrEmpty(num))
+        {
+            Console.WriteLine("Ingrese un numero");
+            num = Console.ReadLine();
+            numero = int.Parse(num);
+            String resultado = (numero > 0) ? "Positivo" : (numero < 0) ? "Negativo" : "Cero";
+
+            //Mostrar el resultado
+            Console.WriteLine("El numero ingresado es: " + resultado);
+
+        }
+        else
+        {
+            numero = int.Parse(num);
+            //Operador termario para determinar si el numero es positivo, negativo o cero.
+            String resultado = (numero > 0) ? "Positivo" : (numero < 0) ? "Negativo" : "Cero";
+
+            //Mostrar el resultado
+            Console.WriteLine("El numero ingresado es: " + resultado);
+        }
+
+        Console.ReadKey();
+    }
+}
