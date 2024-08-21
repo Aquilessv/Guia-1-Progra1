4720296525cb2b0bd54b672280ade273726ee214
Brenda Ivania <bivania706@gmail.com>
2024-08-20 20:26:53 -0600

    Update Ejercicio3.cs

--- Ejercicio3.cs
+++ Ejercicio3.cs
@@ -1,38 +1,36 @@
-﻿class Ejerccio3
+class Ejercicio3
 {
     static void Main(string[] args)
     {
-
-        int nume;
+        string num = string.Empty;
+        int numero;
         bool numeroValido = false;
 
-        while (!numeroValido)
-
-            //solicitar al usuario ingresar un numero
-            Console.WriteLine("Ingrese un numero");
-        string num = Console.ReadLine();
-        int numero;
-        //int numero = int.Parse(Console.ReadLine());
-
-        if (String.IsNullOrEmpty(num))
+        while (numeroValido == false)
         {
-            Console.WriteLine("Ingrese un numero");
+            //solicitar al usuario ingresar un numero
+            Console.WriteLine("Ingrese un numero: \n(No debe estar vacio ni ser letras)");
             num = Console.ReadLine();
-            numero = int.Parse(num);
-            String resultado = (numero > 0) ? "Positivo" : (numero < 0) ? "Negativo" : "Cero";
 
-            //Mostrar el resultado
-            Console.WriteLine("El numero ingresado es: " + resultado);
-
+            //validar si el valor ingresado esta vacio o nulo
+            if (String.IsNullOrEmpty(num)){
+                numeroValido = false;
+            }else{
+                numeroValido = true;
+            }
         }
-        else
+
+        try
         {
             numero = int.Parse(num);
             //Operador termario para determinar si el numero es positivo, negativo o cero.
             String resultado = (numero > 0) ? "Positivo" : (numero < 0) ? "Negativo" : "Cero";
 
             //Mostrar el resultado
-            Console.WriteLine("El numero ingresado es: " + resultado);
+            Console.WriteLine("\nEl numero ingresado es: " + resultado);
+        }
+        catch (Exception error) { 
+            Console.WriteLine("\nERROR DE ENTRADA DE DATOS: " + error.Message);
         }
 
         Console.ReadKey();
